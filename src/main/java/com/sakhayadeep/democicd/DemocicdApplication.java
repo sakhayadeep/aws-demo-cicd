7c34001c6d9a48a8513491f40db83a8784fca779
Sakhayadeep <sakhayadeep.nath@publicissapient.com>
2023-10-15 02:02:26 +0530

    modified DemocicdApplication.java to run on tomcat

--- src/main/java/com/sakhayadeep/democicd/DemocicdApplication.java
+++ src/main/java/com/sakhayadeep/democicd/DemocicdApplication.java
@@ -2,12 +2,19 @@ package com.sakhayadeep.democicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemocicdApplication {
+public class DemocicdApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemocicdApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemocicdApplication.class, args);

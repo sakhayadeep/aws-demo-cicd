48389de92501580b7344259e7cbbc5a8dd5502c4
Sakhayadeep <sakhayadeep.nath@publicissapient.com>
2023-10-15 15:36:04 +0530

    try 2

--- src/main/java/com/sakhayadeep/democicd/DemocicdApplication.java
+++ src/main/java/com/sakhayadeep/democicd/DemocicdApplication.java
@@ -2,27 +2,22 @@ package com.sakhayadeep.democicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
-public class DemocicdApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(DemocicdApplication.class);
-	}
+public class DemocicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemocicdApplication.class, args);
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World!";
+	public List<String> hello() {
+		return List.of("Hello", "World!");
 	}
 
 }
